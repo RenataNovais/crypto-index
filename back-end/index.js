59a85a6d5514284bec5b643b3e6c8194038aa9fa
RenataNovais <rnovaiscs@gmail.com>
2021-08-18 00:22:07 -0300

    Habilitando CORS para todas origens.

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { loginRouter, btcRouter } = require('./controllers');
 
 const PORT = 3001;
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
